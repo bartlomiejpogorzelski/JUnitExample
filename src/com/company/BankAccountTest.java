53afb680f63f487de628d20eae624cb2d43fdbef
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2018-02-13 19:13:29 +0100

    Test methods

---
+++ src/com/company/BankAccountTest.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+    @Test
+    public void deposit() throws Exception {
+        BankAccount account=new BankAccount("Tim", "Buchalka", 1000.00);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+        assertEquals(1200.00, account.getBalance(),0);
+    }
+
+    @Test
+    public void withDraw() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @Test
+    public void getBalance() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+}
